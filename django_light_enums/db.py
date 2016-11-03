60ab87a6c223548c0a9799f015efdc958678fd02
Damian Hites <dhites@makespace.com>
2016-11-03 01:12:01 -0700

    Friendlier migration handling.

--- django_light_enums/db.py
+++ django_light_enums/db.py
@@ -8,18 +8,19 @@ class EnumField(IntegerField):
     Implements storage of an enum value in a db field
     """
 
-    def __init__(self, enum, *args, **kwargs):
-        kwargs['choices'] = enum.choices
-        kwargs.setdefault('default', min(enum.enum_values))
-        self.enum = enum
-        return super(EnumField, self).__init__(*args, **kwargs)
+    def __init__(self, enum=None, enum_values=None, choices=None, default=None, *args, **kwargs):
+        choices = choices or enum.choices
+        default = default or min(enum.enum_values)
+        self.enum_values = enum_values or enum.enum_values
+        return super(EnumField, self).__init__(choices=choices, default=default, *args, **kwargs)
 
     def pre_save(self, model_instance, add):
+        # Do some validation on the value of the field being stored.
         value = getattr(model_instance, self.attname)
         if value is None:
             if not self.null:
                 raise ValidationError(_('None is not a valid value in this non-null enum.'))
-        elif not self.enum.is_valid_value(value):
+        elif value not in self.enum_values:
             raise ValidationError(
                 _('(%(value)s) is not a valid value in this enum. '
                   'Possible values are: %(values)s.'),
@@ -27,7 +28,7 @@ class EnumField(IntegerField):
                     'value': value,
                     'values': ', '.join(
                         '{} ({})'.format(name, value)
-                        for value, name in self.enum.choices
+                        for value, name in self.choices
                     ),
                 },
             )
@@ -35,6 +36,5 @@ class EnumField(IntegerField):
 
     def deconstruct(self):
         name, path, args, kwargs = super(EnumField, self).deconstruct()
-        args.append(self.enum)
-        kwargs.pop('choices', None)
+        kwargs['enum_values'] = self.enum_values
         return name, path, args, kwargs
